fecdcc535422367da7bf50ba3d834aafaf34f9ba
amber.kulkarni <amber.kulkarni@pubmatic.com>
2018-09-17 12:55:48 +0530

    adding sample filw write code without closing resource

--- src/main/java/com/amber/App.java
+++ src/main/java/com/amber/App.java
@@ -1,13 +1,34 @@
 package com.amber;
 
+import java.io.FileWriter;
+import java.util.UUID;
+
 /**
- * Hello world!
+ * Generate a unique number
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        App obj = new App();
+        System.out.println("Unique ID : " + obj.generateUniqueKey());
+        obj.writeKey(obj.generateUniqueKey());
+    }
+
+    private void writeKey(String string) {
+        try {
+            FileWriter fw = new FileWriter("/tmp/amber_lgtm_test");
+            fw.write("key: " + string);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Success...");
+    }
+
+    public String generateUniqueKey() {
+
+        String id = UUID.randomUUID().toString();
+        return id;
+
     }
 }
